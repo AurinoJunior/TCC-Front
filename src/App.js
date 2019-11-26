a4f0335c6597dc83e36a25143f0433e0e4ad33db
Aurino Geraldo <aurino_aj@hotmail.com>
2019-11-26 10:27:05 -0300

    Eslint

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Topo } from './containers'
+
+function App () {
+  return (
+    <Topo />
+  )
+}
+
+export default App
---
+++ src/containers/Topo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const Topo = () => <p>Ola</p>
+
+export default Topo
