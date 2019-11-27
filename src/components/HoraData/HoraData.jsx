f1e6a2af0e79d530ad9808a2d775ecbc92d19f66
Aurino Geraldo <aurino_aj@hotmail.com>
2019-11-26 21:06:19 -0300

    Adicionando hora e data do moment

--- src/components/HoraData/HoraData.jsx
+++ src/components/HoraData/HoraData.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import Moment from 'moment'
+import 'moment/locale/pt-br'
 
 import HoraDataStyle from './HoraDataStyle'
 
 export default class HoraData extends Component {
   state = {
-    data: '25/11/2019',
-    hora: '11:01'
+    data: '',
+    hora: ''
+  }
+
+  componentDidMount () {
+    const data = Moment().format('ll')
+    const hora = Moment().format('LT')
+    this.setState({ data, hora })
   }
 
   render () {
